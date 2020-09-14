b2eac41dac1280457dc095ddd401c54929c8bb4c
Dmitry Novik <dmitry.novik@gmail.com>
2020-09-14 14:37:34 +1000

    Manipulating with dbContext.ChangeTracker.AutoDetectChangesEnabled to speed up data ingestion

--- SolarRadiationStore/Program.cs
+++ SolarRadiationStore/Program.cs
@@ -18,8 +18,6 @@ namespace SolarRadiationStore
             var parsedForecastData = chunkParser.PopulatePointsDaily<SolradNwpForecast>
                 (allPropertyData, Console.WriteLine);
 
-            Console.WriteLine(parsedForecastData);
-
             // Do stuff with data..
 
             using var dbContext = new SolarRadiationDataContext()
@@ -28,20 +26,47 @@ namespace SolarRadiationStore
                 .WithUser("admin")
                 .WithPassword("Password1?");
 
+            const int SAVE_BATCH_SIZE = 100;
+
             var forecastIngestor = new ForecastIngestor();
             ulong count = 0;
+
+            // This speeds up bulk inserts:
+            dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
+
             foreach (var forecast in parsedForecastData)
             {
                 try
                 {
-                    forecastIngestor.Ingest(forecast, dbContext);
-                    Console.WriteLine($"Processed {++count} rows");
+                    count = forecastIngestor.Ingest(forecast, dbContext, count);
+                    if (count % SAVE_BATCH_SIZE == 0)
+                    {
+                        SaveBatch(dbContext, count);
+                    }
                 }
                 catch (Exception e)
                 {
                     LogError(e);
                 }
             }
+
+            // Save last batch:
+            SaveBatch(dbContext, count);
+        }
+
+        private static void SaveBatch(SolarRadiationDataContext dbContext, ulong count)
+        {
+            try
+            {
+                dbContext.ChangeTracker.AutoDetectChangesEnabled = true;
+                dbContext.SaveChanges();
+                Console.WriteLine($"Processed {count} rows");
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
+            dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
         }
 
         private static void LogError(Exception e)
